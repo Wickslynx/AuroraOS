af42d12244364567162b1f24eb110347a4a73f1b
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2024-12-30 20:41:16 +0100

    Added introduction message.

--- mem/memory.h
+++ mem/memory.h
@@ -1,3 +1,21 @@
+//----------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------
+//Please see the contributer_introduction file for more info on how to commit!
+
+/*   --Memory related systems built for AuroraOS--
+
+
+Important stuff to know:
+
+More stuff added in the future.
+
+
+
+*/ 
+
+//----------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------
+
 #include "kernel.h"
 
 void memset(void *s, int c, unsigned int n);
