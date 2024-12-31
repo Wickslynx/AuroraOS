d21c639db52823ec3723a743846f477c62574a2f
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2024-12-31 12:27:04 +0100

    Update kernel_entry.asm

--- asm/kernel_entry.asm
+++ asm/kernel_entry.asm
@@ -1,11 +1,25 @@
 [bits 32]
-[external kernel_main]
+[extern kernel_main]    ; Declare kernel_main as external
 
-section .txt
+section .text          ; Fix the typo from .txt to .text
 global _start
-
 _start:
-  cli    ; Clear inputs.
-  mov esp, 0x90000    ; Make stack.
-  call kernel_main  ;  Call the kernel_main function.
-  hlt ; Halt cpu.
+    ; Set up stack properly
+    mov esp, kernel_stack_top
+    
+    ; Clear EFLAGS
+    push 0
+    popf
+    
+    ; Call kernel_main
+    call kernel_main
+    
+    ; If kernel returns, halt
+    cli
+    hlt
+
+section .bss
+align 4
+kernel_stack_bottom:
+    resb 16384  ; 16 KB for stack
+kernel_stack_top:
