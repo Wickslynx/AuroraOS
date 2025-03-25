e1ffe77e18ba0ee6865d52ab02bdc16e41b1a8b7
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-03-25 15:59:14 +0100

    Fixed stuff, hope this works.

--- fs/file_sys.h
+++ fs/file_sys.h
@@ -1,31 +1,45 @@
-#ifdef FILE_SYS
-#define FILE_SYS
+#ifndef FILE_SYS_H
+#define FILE_SYS_H
 
-#define FS_MAGIC 0x19383845
-#define FS_BLOCK_SIZE 512
-#define FS_INODE_COUNT 128
+#include "macros.h"
+#include "memory.h"
 
-typedef struct superblock {
-  unsigned int magic;
-  unsigned int total_blocks;
-  unsigned int inode_count;
-  unsigned int data_block_count;
+extern void memset(void *s, int c, unsigned int n);
+
+extern void memcpy(void *dest, void *src, unsigned int n);
+
+
+#define BLOCK_SIZE 512
+#define MAX_FILES 256
+#define MAX_FILENAME_LENGTH 32
+#define FS_MAGIC 0xABCD1234
+
+// Superblock Structure
+typedef struct {
+    uint32_t magic;           // Magic number to identify filesystem
+    uint32_t total_blocks;    // Total number of blocks
+    uint32_t inode_blocks;    // Number of inode blocks
+    uint32_t data_blocks;     // Number of data blocks
+    uint32_t free_inode_bitmap; // Position of inode bitmap
+    uint32_t free_data_bitmap;  // Position of data bitmap
 } superblock_t;
 
-typedef struct inode {
-  unsigned int size;
-  unsigned int data_block_indices;
+// Inode Structure
+typedef struct {
+    uint32_t size;                 // File size
+    uint32_t data_block_indices[12]; // Indices of data blocks
 } inode_t;
 
-typedef struct dir_entry {
-  char filename[32];
-  unsigned int inode_index;
+// Directory Entry Structure
+typedef struct {
+    char filename[MAX_FILENAME_LENGTH]; // Filename
+    uint32_t inode_index;               // Index of corresponding inode
 } dir_entry_t;
 
+// Function Prototypes
 void fs_init();
-void create();
-void write();
-void read();
+int create(const char *filename);
+int write(const char *filename, const char *data, unsigned int length);
+int read(const char *filename, char *buffer, unsigned int length);
 
-
-#endif 
+#endif // FILE_SYS_H
