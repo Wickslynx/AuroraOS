32b7d54489b805581e1c986cf71b8ed7631a7df2
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-03-25 13:20:20 +0100

    fix.

---
+++ frontend/frontend.h
@@ -0,0 +1,9 @@
+#ifndef FRONTEND_H
+#define FRONTEND_H
+
+external static void update_cursor(int x, int y);
+
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+
+#endif //FRONTEND_H
