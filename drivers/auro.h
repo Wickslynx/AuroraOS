0195a25b00ee56c2485953b2b833ce4d4db5a4e4
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-03-26 11:11:01 +0100

    adwa

--- drivers/auro.h
+++ drivers/auro.h
@@ -1,1 +1,30 @@
-// ---- AuroraOS's windowing system ----
+//----------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------
+//Please see the contributer_introduction file for more info on how to commit!
+
+/*  ---- AuroraOS's windowing system ----
+
+
+Important stuff to know:
+
+Color values are declared in:
+utils/macros/macros.h
+
+Ground Up I/O is defined in io/io.h
+
+Update cursor func declared in:
+utils/funcs/functions.h
+
+More stuff added in the future.
+
+
+
+*/ 
+
+//----------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------
+
+#include "io.h"
+
+
+
