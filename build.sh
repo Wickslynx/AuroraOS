a94622df6dcf29c2faf7026c3ff2d7a5b6d67851
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-03-26 18:39:40 +0100

    Fixed small thing

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-
-# This script will build and run your AuroraOS with QEMU.
-
+# This script will build and run AuroraOS with QEMU.
 # --- Configuration ---
 BOOT_ASM="kernel/asm/boot.asm"
 KERNEL_ENTRY_ASM="kernel/asm/kernel_entry.asm"
@@ -16,8 +14,8 @@ LINKER_SCRIPT="linker.ld"
 OUTPUT_BOOT_BIN="boot.bin"
 OUTPUT_KERNEL_O="kernel_entry.o"
 OUTPUT_FRONTEND_O="home.o"
-OUTPUT_FILESYS_O="file_sys.o"   # Object file for file system
-OUTPUT_MEMORY_O="memory.o"     # Object file for memory management
+OUTPUT_FILESYS_O="file_sys.o"
+OUTPUT_MEMORY_O="memory.o"
 OUTPUT_KERNEL_ELF="kernel.elf"
 OUTPUT_KERNEL_BIN="kernel.bin"
 OUTPUT_OS_BIN="auroraos.bin"
@@ -31,41 +29,37 @@ if [ $? -ne 0 ]; then
 fi
 
 echo "Assembling kernel entry..."
-nasm "$KERNEL_ENTRY_ASM" -f elf -o "$OUTPUT_KERNEL_O"
+nasm "$KERNEL_ENTRY_ASM" -f elf64 -o "$OUTPUT_KERNEL_O"
 if [ $? -ne 0 ]; then
     echo "Error assembling kernel entry. Exiting."
     exit 1
 fi
 
-
 # --- Compilation ---
-
 echo "Compiling memory management..."
-gcc -m64 -c -ffreestanding -nostdlib -I kernel.h -I frontend -I utils/macros -I utils/funcs -I mem -I init -I fs -o "$OUTPUT_MEMORY_O" "$MEMORY_C"
+gcc -m64 -mcmodel=kernel -mno-red-zone -c -ffreestanding -nostdlib -I kernel.h -I frontend -I utils/macros -I utils/funcs -I mem -I init -I fs -o "$OUTPUT_MEMORY_O" "$MEMORY_C"
 if [ $? -ne 0 ]; then
     echo "Error compiling memory management. Exiting."
     exit 1
 fi
 
 echo "Compiling frontend..."
-gcc -m64 -c -ffreestanding -nostdlib -I frontend -I utils/macros -I mem -I utils/funcs -I init -I fs -o "$OUTPUT_FRONTEND_O" "$FRONTEND_C"
+gcc -m64 -mcmodel=kernel -mno-red-zone -c -ffreestanding -nostdlib -I frontend -I utils/macros -I mem -I utils/funcs -I init -I fs -o "$OUTPUT_FRONTEND_O" "$FRONTEND_C"
 if [ $? -ne 0 ]; then
     echo "Error compiling frontend. Exiting."
     exit 1
 fi
 
 echo "Compiling file system..."
-gcc -m64 -c -ffreestanding -nostdlib -I frontend -I utils/macros -I mem -I utils/funcs -I init -I fs -o "$OUTPUT_FILESYS_O" "$FILESYS_C"
+gcc -m64 -mcmodel=kernel -mno-red-zone -c -ffreestanding -nostdlib -I frontend -I utils/macros -I mem -I utils/funcs -I init -I fs -o "$OUTPUT_FILESYS_O" "$FILESYS_C"
 if [ $? -ne 0 ]; then
     echo "Error compiling file system. Exiting."
     exit 1
 fi
 
-
-
 # --- Linking ---
 echo "Linking kernel..."
-ld -T "$LINKER_SCRIPT" "$OUTPUT_KERNEL_O" "$OUTPUT_FRONTEND_O" "$OUTPUT_FILESYS_O" "$OUTPUT_MEMORY_O" -o "$OUTPUT_KERNEL_ELF"
+ld -m elf_x86_64 -T "$LINKER_SCRIPT" "$OUTPUT_KERNEL_O" "$OUTPUT_FRONTEND_O" "$OUTPUT_FILESYS_O" "$OUTPUT_MEMORY_O" -o "$OUTPUT_KERNEL_ELF"
 if [ $? -ne 0 ]; then
     echo "Error linking kernel. Exiting."
     exit 1
