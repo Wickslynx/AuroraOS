abc2c578a608105a2b92aa9308f0163f82db66cd
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-03-26 08:22:12 +0100

    Fix.

--- asm/kernel_entry.asm
+++ asm/kernel_entry.asm
@@ -1,22 +1,23 @@
-[bits 32]
-[extern kernel_main]    ; kernel.c
+[bits 64] ; Wicks, 2025: Rewritten for 64 bit architecture.
+[extern kernel_main]    
 
-section .text          
+section .text                ; This section is for the executable code of your kernel.
 global _start
 _start:
-    mov esp, kernel_stack_top ; Set up stack.
-    
-     
-    push 0  ; Clear the EFLAGS (Whatever that is.)
-    popf
-    
-    
-    call kernel_main ; Call kernel_main func.
-    cli ; If kernel returns, halt
-    hlt
+    mov rsp, kernel_stack_top ; Set up the stack pointer (rsp) to the top of our kernel stack. 
+    ; Clear the RFLAGS register.
+    push 0
+    popfq ; 
+
+    ; Call the kernel_main function (kernel.c).
+    call kernel_main
+
+    ; If kernel_main were to return, we would disable interrupts and halt the processor.
+    cli ;
+    hlt ; 
 
 section .bss
-align 4
+align 8 ; Align the following data on an 8-byte boundary (good for 64-bit).
 kernel_stack_bottom:
-    resb 32768  ; 32 KB for stack, good enough for now.
-kernel_stack_top:
+    resb 65536      ; Reserve 65536 bytes (64 KB) of memory for the kernel stack
+kernel_stack_top:    ; This label marks the top of the kernel stack (stack grows downwards).
