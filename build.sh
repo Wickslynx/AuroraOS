c3b7eec481038dbb6ad08b8fbf55aeea5d73842c
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-03-26 08:23:35 +0100

    Changed to 64 bit (Changed all compile flags)

--- build.sh
+++ build.sh
@@ -42,21 +42,21 @@ fi
 # --- Compilation ---
 
 echo "Compiling memory management..."
-gcc -m32 -c -ffreestanding -nostdlib -I kernel.h -I frontend -I utils/macros -I utils/funcs -I mem -I init -I fs -o "$OUTPUT_MEMORY_O" "$MEMORY_C"
+gcc -m64 -c -ffreestanding -nostdlib -I kernel.h -I frontend -I utils/macros -I utils/funcs -I mem -I init -I fs -o "$OUTPUT_MEMORY_O" "$MEMORY_C"
 if [ $? -ne 0 ]; then
     echo "Error compiling memory management. Exiting."
     exit 1
 fi
 
 echo "Compiling frontend..."
-gcc -m32 -c -ffreestanding -nostdlib -I frontend -I utils/macros -I mem -I utils/funcs -I init -I fs -o "$OUTPUT_FRONTEND_O" "$FRONTEND_C"
+gcc -m64 -c -ffreestanding -nostdlib -I frontend -I utils/macros -I mem -I utils/funcs -I init -I fs -o "$OUTPUT_FRONTEND_O" "$FRONTEND_C"
 if [ $? -ne 0 ]; then
     echo "Error compiling frontend. Exiting."
     exit 1
 fi
 
 echo "Compiling file system..."
-gcc -m32 -c -ffreestanding -nostdlib -I frontend -I utils/macros -I mem -I utils/funcs -I init -I fs -o "$OUTPUT_FILESYS_O" "$FILESYS_C"
+gcc -m64 -c -ffreestanding -nostdlib -I frontend -I utils/macros -I mem -I utils/funcs -I init -I fs -o "$OUTPUT_FILESYS_O" "$FILESYS_C"
 if [ $? -ne 0 ]; then
     echo "Error compiling file system. Exiting."
     exit 1
@@ -86,4 +86,4 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-echo "Done! You can now run AuroraOS with qemu using: qemu-system-i386 -fda $OUTPUT_OS_BIN"
+echo "Done! You can now run AuroraOS with qemu using: qemu-system-x86_64 -fda $OUTPUT_OS_BIN"
