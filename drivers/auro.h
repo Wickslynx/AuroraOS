6b2c314bd198b4195527eae5b5a1e0f4a9601800
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-03-27 18:51:15 +0100

    Fix.-

--- drivers/auro.h
+++ drivers/auro.h
@@ -25,6 +25,44 @@ More stuff added in the future.
 //----------------------------------------------------------------------------------------
 
 #include "io.h"
+#include "kernel/fb/fb.h"
+
+typdef struct {
+  uint16 id; //WINDOW ID
+  uint16 x, y; //X, Y
+  uint16 width, heigh; //HEIGHT, WIDTH
+  Pixel *buffer; //Framebuffer
+  uint8 z; //Layering (How it is gonna be drawn)
+} Window;
+  
+typedef struct {
+  uint16 id,
+  Window windows[MAX_WIN]
+  uint8 w_count
+  char name[64];
+} AuroApplication;
+
+
+void AuroWindowCreate() {
+  
+}
+
+
+void AuroDisplayOpen() {
+    
+}
+
+void AuroEventProcess() {
+  
+} 
+
+void AuroSwapBuffers() {
+  
+}
+
+
+
+
 
 
 
