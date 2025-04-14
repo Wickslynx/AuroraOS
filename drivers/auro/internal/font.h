b4b73bbf9c93046557ece70e2301272df8981282
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-04-14 13:08:47 +0200

    adawd

--- drivers/auro/internal/font.h
+++ drivers/auro/internal/font.h
@@ -15,7 +15,7 @@
         font_str(__s, __x, __y, __c);\
     } while (0);
 
-void font_char(char c, size_t x, size_t y, u8 color);
-void font_str(const char *s, size_t x, size_t y, u8 color);
+void font_char(char c, size_t x, size_t y, uint8 color);
+void font_str(const char *s, size_t x, size_t y, uint8 color);
 
 #endif
