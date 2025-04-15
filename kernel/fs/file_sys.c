01ef8351811ce550b50dcfbcec6899367522a3ff
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-04-15 13:13:50 +0200

    Fix

--- kernel/fs/file_sys.c
+++ kernel/fs/file_sys.c
@@ -10,26 +10,17 @@
 
 Important stuff to know:
 
-Superblock structure:
-Total blocks: 1024
-Inode blocks: 16
-Data blocks: 1008
+Latest update:
+Revamped the FS to follow a dynamic approach instead.
 
 
 Global variables:
-- fs_memory, Main array thats holding the whole filesystem.
-- sb, ptr to superblock.
-- inodes, ptr to the inode table.
-- data_blocks, ptr to the data storage.
-- free_inode_bitmap, Tracks inode allocation
-- free_data_bitmap, Tracks data allocation.
+
 
 Memory layout: 
 [Superblock][Inode Bitmap][Data Bitmap][Inode Table][Data Blocks]
 
 Additional declarations:
-
-memcpy, memset: mem/memory.c
 true, false: utils/macros/macros.c
 
 
@@ -37,57 +28,58 @@ true, false: utils/macros/macros.c
 
 //----------------------------------------------------------------------------------------
 //----------------------------------------------------------------------------------------
-unsigned char fs_memory[BLOCK_SIZE * 1024]; // 512KB filesystem
 
-// Pointers to filesystem sections
+#include "file_sys.h"
+#include "../utils/utils.h"
+
+#define FS_BASE_ADDRESS 0x200000
+#define FS ((unsigned char *)FS_BASE_ADDRESS)
+
 superblock_t *sb;
 inode_t *inodes;
 unsigned char *data_blocks;
 unsigned char *free_inode_bitmap;
 unsigned char *free_data_bitmap;
-typedef char bool; // TO prevent error
+
+typedef char bool;
 
 void fs_init() {
-    sb = (superblock_t *)fs_memory; 
+    sb = (superblock_t *)FS;
     sb->magic = FS_MAGIC;
 
     sb->total_blocks = 1024;
     sb->inode_blocks = 16;
     sb->data_blocks = 1008;
-    
+
     sb->free_inode_bitmap = sizeof(superblock_t);
     sb->free_data_bitmap = sb->free_inode_bitmap + sb->inode_blocks;
-    
-    inodes = (inode_t *)(fs_memory + sb->free_inode_bitmap + sb->inode_blocks);
-    data_blocks = fs_memory + BLOCK_SIZE * (sb->free_data_bitmap + sb->data_blocks);
-    free_inode_bitmap = fs_memory + sb->free_inode_bitmap;
-    free_data_bitmap = fs_memory + sb->free_data_bitmap;
 
-    // Use new memset
+    inodes = (inode_t *)(FS + sb->free_inode_bitmap + sb->inode_blocks);
+    data_blocks = FS + BLOCK_SIZE * (sb->free_data_bitmap + sb->data_blocks);
+    free_inode_bitmap = FS + sb->free_inode_bitmap;
+    free_data_bitmap = FS + sb->free_data_bitmap;
+
     OSmemset(free_inode_bitmap, 0, sb->inode_blocks);
     OSmemset(free_data_bitmap, 0, sb->data_blocks);
 }
 
-int create(const char *filename) {        
+int create(const char *filename) {
     for (unsigned int i = 0; i < MAX_FILES; i++) {
         if (!free_inode_bitmap[i]) {
             free_inode_bitmap[i] = 1;
-            
             inode_t *new_inode = &inodes[i];
             OSmemset(new_inode, 0, sizeof(inode_t));
-            
+
             dir_entry_t *new_entry = (dir_entry_t *)(data_blocks + i * BLOCK_SIZE);
-            
             for (int j = 0; j < MAX_FILENAME_LENGTH; j++) {
                 new_entry->filename[j] = filename[j];
                 if (filename[j] == '\0') break;
             }
-            
             new_entry->inode_index = i;
             return 0;
         }
     }
-    return -1; 
+    return -1;
 }
 
 int write(const char *filename, const char *data, unsigned int length) {
@@ -95,7 +87,6 @@ int write(const char *filename, const char *data, unsigned int length) {
 
     for (unsigned int i = 0; i < MAX_FILES; i++) {
         dir_entry_t *entry = (dir_entry_t *)(data_blocks + i * BLOCK_SIZE);
-        
         bool found = true;
         for (int j = 0; j < MAX_FILENAME_LENGTH; j++) {
             if (entry->filename[j] != filename[j]) {
@@ -104,39 +95,30 @@ int write(const char *filename, const char *data, unsigned int length) {
             }
             if (filename[j] == '\0') break;
         }
-        
+
         if (found) {
-            inode_t *file_inode = &inodes[entry->inode_index];
-            
+            inode_t *inode = &inodes[entry->inode_index];
             if (length > BLOCK_SIZE * 12) return -1;
-            
+
             unsigned int blocks_needed = (length + BLOCK_SIZE - 1) / BLOCK_SIZE;
-            
-            unsigned int free_blocks_count = 0;
-            for (unsigned int k = 0; k < MAX_DATA_BLOCKS; k++) {
-                if (!free_data_bitmap[k]) free_blocks_count++;
-            }
-            
-            if (free_blocks_count < blocks_needed) return -1;
-            
             unsigned int blocks_written = 0;
+
             for (unsigned int j = 0; j < MAX_DATA_BLOCKS && blocks_written < blocks_needed; j++) {
                 if (!free_data_bitmap[j]) {
                     free_data_bitmap[j] = 1;
-                    file_inode->data_block_indices[blocks_written] = j;
-                    
-                    unsigned int bytes_to_copy = (blocks_written == blocks_needed - 1) 
-                        ? (length % BLOCK_SIZE ? length % BLOCK_SIZE : BLOCK_SIZE) 
+                    inode->data_block_indices[blocks_written] = j;
+
+                    unsigned int bytes_to_copy = (blocks_written == blocks_needed - 1)
+                        ? (length % BLOCK_SIZE ? length % BLOCK_SIZE : BLOCK_SIZE)
                         : BLOCK_SIZE;
-                    
+
                     OSmemcpy(data_blocks + j * BLOCK_SIZE, data + blocks_written * BLOCK_SIZE, bytes_to_copy);
-                    
                     blocks_written++;
                 }
             }
-            
-            file_inode->size = length;
-            return 0; 
+
+            inode->size = length;
+            return 0;
         }
     }
     return -1;
@@ -145,7 +127,6 @@ int write(const char *filename, const char *data, unsigned int length) {
 int read(const char *filename, char *buffer, unsigned int length) {
     for (unsigned int i = 0; i < MAX_FILES; i++) {
         dir_entry_t *entry = (dir_entry_t *)(data_blocks + i * BLOCK_SIZE);
-        
         bool found = true;
         for (int j = 0; j < MAX_FILENAME_LENGTH; j++) {
             if (entry->filename[j] != filename[j]) {
@@ -154,17 +135,19 @@ int read(const char *filename, char *buffer, unsigned int length) {
             }
             if (filename[j] == '\0') break;
         }
-        
+
         if (found) {
-            inode_t *file_inode = &inodes[entry->inode_index];
-            length = file_inode->size;
+            inode_t *inode = &inodes[entry->inode_index];
+            length = inode->size;
 
             for (unsigned int j = 0; j < (length + BLOCK_SIZE - 1) / BLOCK_SIZE; j++) {
-                OSmemcpy(buffer + j * BLOCK_SIZE, data_blocks + file_inode->data_block_indices[j] * BLOCK_SIZE, BLOCK_SIZE);
+                OSmemcpy(buffer + j * BLOCK_SIZE,
+                         data_blocks + inode->data_block_indices[j] * BLOCK_SIZE,
+                         BLOCK_SIZE);
             }
-            
             return length;
         }
     }
     return -1;
 }
+
