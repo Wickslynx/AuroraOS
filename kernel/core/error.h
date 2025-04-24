4c4b1ba3abcd1b66a5a5d747cc86e1d799e10e2c
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-04-24 20:23:19 +0200

    Fixed

--- kernel/core/error.h
+++ kernel/core/error.h
@@ -3,5 +3,16 @@
 
 #include "system.h
 
+void panic(const char *err) {
+    screen_clear(COLOR(7, 0, 0));
+
+    if (err != NULL) {
+        font_str(err, (SCREEN_WIDTH - font_width(err)) / 2, SCREEN_HEIGHT / 2 - 4, COLOR(7, 7, 3));
+    }
+
+    screen_swap();
+    for (;;) {}
+}
+
 
 #endif //ERROR_H
