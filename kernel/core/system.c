367458c7da33ddb057cc6f7bdf29c65e6c64daae
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-04-24 20:23:02 +0200

    Fix

--- kernel/core/system.c
+++ kernel/core/system.c
@@ -23,13 +23,3 @@ u32 rand() {
     return w = w ^ (w >> 19) ^ t ^ (t >> 8);
 }
 
-void panic(const char *err) {
-    screen_clear(COLOR(7, 0, 0));
-
-    if (err != NULL) {
-        font_str(err, (SCREEN_WIDTH - font_width(err)) / 2, SCREEN_HEIGHT / 2 - 4, COLOR(7, 7, 3));
-    }
-
-    screen_swap();
-    for (;;) {}
-}
