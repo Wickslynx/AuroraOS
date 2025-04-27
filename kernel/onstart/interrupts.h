41d83016f5bbda2c662bca41292505d47491b3c8
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-04-27 18:00:37 +0200

    Fixed

--- kernel/onstart/interrupts.h
+++ kernel/onstart/interrupts.h
@@ -1,36 +1,13 @@
 #ifndef INTERRUPTS_H
 #define INTERRUPTS_H
 
-#include <stddef.h>
-#include <stdint.h>
 
-// Common types
-typedef uint8_t u8;
-typedef uint16_t u16;
-typedef uintptr_t uintptr_t;
 
-// Macros for atomic operations
-#define CLI() asm volatile("cli")
-#define STI() asm volatile("sti")
-#define PACKED __attribute__((packed))
-
-// Port I/O functions
-static inline u8 inportb(u16 port) {
-    u8 ret;
-    asm volatile("inb %1, %0" : "=a"(ret) : "Nd"(port));
-    return ret;
-}
-
-static inline void outportb(u16 port, u8 val) {
-    asm volatile("outb %0, %1" : : "a"(val), "Nd"(port));
-}
-
-// Registers structure for interrupt handlers
 struct Registers {
-    uint32_t ds;
-    uint32_t edi, esi, ebp, esp, ebx, edx, ecx, eax;
-    uint32_t int_no, err_code;
-    uint32_t eip, cs, eflags, useresp, ss;
+    u32 ds;
+    u32 edi, esi, ebp, esp, ebx, edx, ecx, eax;
+    u32 int_no, err_code;
+    u32 eip, cs, eflags, useresp, ss;
 };
 
 // FPU functions
