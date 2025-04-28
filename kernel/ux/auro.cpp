e36c7169f9f2e6844a8ed78cbc737557b8f2054b
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-04-28 19:31:10 +0200

    Fix

--- kernel/ux/auro.cpp
+++ kernel/ux/auro.cpp
@@ -13,11 +13,17 @@ Note: Everything under the "Internal" namespace is internal...
 Application developers, when you use these functions you are writing to the framebuffer, and not your application.
 Don't use them.
 
+Everything under 
 --------------------
 */
 
 namespace Auro {
+
+
 WindowManager* WindowManager::instance = nullptr;
+
+
+//  Func defs
 
 void Internal::drawRectangle(u16 color, int x, int y, int width, int height) {
     for (int j = 0; j < height; j++) {
@@ -28,6 +34,8 @@ void Internal::drawRectangle(u16 color, int x, int y, int width, int height) {
 void Internal::drawText(const char* text, int x, int y, u8 color) {
     font_str(text, x, y, color);
 }
+
+
 
 void Window::create(const char* windowName, int posX, int posY, int windowHeight, int windowWidth) {
     // free previous name if it exits.
@@ -40,7 +48,7 @@ void Window::create(const char* windowName, int posX, int posY, int windowHeight
     name = new char[nameLength];
     strcpy(name, windowName);
     
-    // set window properties
+    // set window props
     x = posX;
     y = posY;
     height = windowHeight;
@@ -81,9 +89,9 @@ bool WindowManager::addWindow(Window* window) {
     return true;
 }
 
-void initialize() {
-    WindowManager::initialize();
-}
+
+
+//  Public functions without any classes.
 
 Window* createWindow(const char* name, int x, int y, int height, int width) {
     Window* window = new Window();
