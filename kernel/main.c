02fff85f95de7fdce0691af447dc0edf56d00554
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-05-15 16:25:24 +0200

    Fix

--- kernel/main.c
+++ kernel/main.c
@@ -1,9 +1,15 @@
 #include "ux/system/screen.h"
+#include "core/interrupts.h"
 
 
 
 
 void _main(u32 magic) {
+    idt_init();
+    isr_init();
+    fpu_init(); // floating point unit.
+    irq_init();
+    
     screen_init();
 
     while (true) {
