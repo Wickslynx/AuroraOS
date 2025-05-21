2336f9736abf871a9a1b08716315294f24065d55
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-05-21 10:36:12 +0200

    Fix

---
+++ kernel/mem/mem.c
@@ -0,0 +1,15 @@
+void* operator new(unsigned int size) {
+  return;
+}
+
+void* operator new[](unsigned int size) {
+  return;
+}
+
+void operator delete(void* ptr) {
+  return;
+}
+
+void operator delete[](void* ptr) {
+  return;
+}
