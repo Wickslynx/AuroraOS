d0caf951ad44b1b00b80c84d3fd2da6b6b442212
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-05-21 14:07:21 +0200

    wadw

--- kernel/fs/fs.h
+++ kernel/fs/fs.h
@@ -1,6 +1,10 @@
 #ifndef FS_H
 #define FS_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include "../core/util.h"
 
 int write(const char* name, int size, int data);
@@ -8,6 +12,10 @@ int read(const char* name);
 
 int mkdir(const char* name);
 int rm(const char* name);
+
+#ifdef __cplusplus
+}
+#endif
 
 
 #endif
