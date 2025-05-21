e18177533220c946499d75512efb5a46f44932f4
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-05-21 14:05:41 +0200

    Fix

--- kernel/fs/fs.h
+++ kernel/fs/fs.h
@@ -1,1 +1,13 @@
+#ifndef FS_H
+#define FS_H
 
+#include "../core/util.h"
+
+int write(const char* name, int size, int data);
+int read(const char* name);
+
+int mkdir(const char* name);
+int rm(const char* name);
+
+
+#endif
