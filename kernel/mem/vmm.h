0b0f405c0ba3d93f12a493a2b3c6783affbacf4d
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-05-23 20:40:40 +0200

    Fi

--- kernel/mem/vmm.h
+++ kernel/mem/vmm.h
@@ -10,7 +10,7 @@ extern "C" {
 typedef struct {
   u32 present : 1; // page in mem
   u32 rw : 1; // read/write perms
-  u32 user : 1; // user/sudo privs
+  u32 user : 1; // user/supervisor privs
   u32 pwt : 1; // write through
   u32 pcd : 1; //chache disabled
   u32 accesed : 1; // has been accesed
@@ -18,7 +18,7 @@ typedef struct {
   u32 pat : 1; // page attribute tabel
   u32 global : 1; //global, not flushed
   u32 available : 3; // can be used
-  u32 frame : 30; // frame adress
+  u32 frame : 39; // frame adress
 
 }  page_t;
 
