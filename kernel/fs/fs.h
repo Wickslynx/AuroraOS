495c53798571d6a7dd1b3c4f1dc2fee1a319d21e
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-05-23 10:10:58 +0200

    Fix

--- kernel/fs/fs.h
+++ kernel/fs/fs.h
@@ -5,7 +5,10 @@
 extern "C" {
 #endif
 
+#define FILE int
+
 #include "../core/util.h"
+#include "../core/error.h" 
 
 int write(const char* name, int size, int data);
 int read(const char* name);
@@ -13,9 +16,16 @@ int read(const char* name);
 int mkdir(const char* name);
 int rm(const char* name);
 
+typedef enum {
+  DATA, 
+  SIZE,
+  PERMS
+} file;
+
 #ifdef __cplusplus
 }
 #endif
+
 
 
 #endif
