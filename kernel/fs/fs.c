cb418113d91c4fdf3085cc8ff70159c3f51fb959
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-05-23 10:10:39 +0200

    Fixe

--- kernel/fs/fs.c
+++ kernel/fs/fs.c
@@ -1,2 +1,30 @@
 #include "fs.h"
 
+int __open(const char* name, file type) {
+  switch (type) {
+    case DATA:
+      __read(fp);
+      break;
+    case SIZE:
+      break;
+    default:
+      panic("Filesystem called with an unsupported type"); // This should probably be a warning, for now i'll leave it as a panic.
+      break;
+  }
+}
+
+int write(const char* name, int data) {
+  int dsize = sizeof(data);
+  int data = __open(name, file.DATA);
+}
+
+int read(const char* name) {
+  return __open(name, file.DATA);
+}
+
+int mkdir(const char* name) {
+  
+}
+int rm(const char* name) {
+  
+}
