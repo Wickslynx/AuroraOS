f99a715fcaba0612374e0c66fc4c937516cc4a4b
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-05-26 12:04:52 +0200

    Fixed

--- kernel/mem/vmm.c
+++ kernel/mem/vmm.c
@@ -24,7 +24,7 @@ void imap_kernel() {
   
 }
 
-void vmalloc(u32 size) {
+int vmalloc(u32 size) {
     heap_ptr = (heap_ptr + 0xFFF) & ~0xFFF; // align heap pointer -> boundary
     
     void* addr = (void*)heap_ptr;
