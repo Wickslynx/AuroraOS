a8727aac34865a470437bd2842c90f1b72f3fb42
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-05-28 15:53:55 +0200

    Added a window

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -7,20 +7,27 @@
 #include "core/error.h"
 
 
+void test() {
+    Auro::Window window;
+    window.create("Test", 10, 10, SCREEN_HEIGHT - 20, SCREEN_WIDTH - 20);
+}
     
 extern "C" void _main(u32 magic) {
     idt_init();
     isr_init();
     fpu_init(); // floating point unit.
     irq_init();
-    // imap_kernel(); Smth is wrong with this, can't care enough to fix it for now so ill js comment it out.
+    /*
+    imap_kernel();
     init_vmm();
+    */
     screen_init();
 
     bool quit = false;
     
     while (quit != true) {
         screen_clear(COLOR(0, 255, 0));
+	test();
         screen_swap();
     }
 
