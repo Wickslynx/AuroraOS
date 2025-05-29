eaa8f568ef32ab08c410087eb15dfafacce35716
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-05-29 21:00:12 +0200

    Fox

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -5,7 +5,7 @@
 #include "core/util.h"
 #include "mem/vmm.h"
 #include "core/error.h"
-#include "../terminal/terminal.cpp"
+#include "../apps/terminal/terminal.cpp"
 
 void test() {
     terminal();
