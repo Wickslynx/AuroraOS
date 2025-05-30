24dce59b62e9fe3624418076fa809f11a5b4c509
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-05-30 11:27:49 +0200

    Fix

--- apps/terminal/terminal.cpp
+++ apps/terminal/terminal.cpp
@@ -1,14 +1,38 @@
 #include "../../kernel/ui/auro.h"
 #include "../../kernel/drivers/keyboard.h"
+#include "../../kernel/core/util.h"
+
+
+void runcmd(const char *cmd) {
+  return;
+}
+
 
 int terminal() {
   Auro::Window window;
   int width = SCREEN_WIDTH - 20;
   int height = SCREEN_HEIGHT - 30;
+  int twidth =  (width - font_width("user@aurora - $ ")) / 20; // Starting width
+  char cmd[1024]; //
+  int len = 0;
   
   window.create("Aurora Terminal", 10, 10, height, width);
 
   window.clear(COLOR(0, 0, 0));
-  window.text("user@aurora - $ ", (width - font_width("user@aurora - $")) / 20, 10, COLOR(0, 7, 0));
-  return 0;
+  window.text("user@aurora - $ ", twidth, 10, COLOR(0, 7, 0));
+
+  char key = stoc(keyboard_get_last_char());
+  
+  
+  if (key == 'KEY_ENTER') {
+    runcmd(cmd); // placeholder
+  } else {
+    len++;
+    cmd[len] = key;
+    char buffer[1];
+    itoa(key, buffer, sizeof(key));
+    font_str(buffer, twidth+3, 10, COLOR(7, 7, 3));
+  }
+  
+
 }
