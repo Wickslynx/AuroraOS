ddf80bfe772baea0f1eb01f51f93e8e7bd8fd8de
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-06-03 15:40:12 +0200

    Fix

--- kernel/mem/mem.h
+++ kernel/mem/mem.h
@@ -1,11 +1,6 @@
 #ifndef MEM_H
 #define MEM_H
 
-void* operator new(unsigned int size);
-void* operator new[](unsigned int size);
-void operator delete(void* ptr);
-void operator delete[](void* ptr);
-
 void* malloc(u32 size);
 void free(void* ptr);
 #endif
