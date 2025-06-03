e7d7fa5edcaf3d27d558ddd77306ec2248b0e3e6
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-06-03 15:19:58 +0200

    Fix

--- kernel/ui/system/screen.h
+++ kernel/ui/system/screen.h
@@ -8,41 +8,57 @@ extern "C" {
 #include "../../core/util.h"
 #include "../../core/error.h"
 
-#define SCREEN_WIDTH 1280
-#define SCREEN_HEIGHT 720
-#define BPP 2
+
+#define SCREEN_WIDTH 1024
+#define SCREEN_HEIGHT 768
+#define BPP 2  // 16-bit = 2 bytes per pixel
 #define SCREEN_SIZE (SCREEN_WIDTH * SCREEN_HEIGHT * BPP)
 
 
-
-
-extern u8 _sbuffers[2][SCREEN_SIZE];
+extern u8 *sbuffers[2];
 extern u8 _sback;
 
-#define screen_buffer() (_sbuffers[_sback])
 
-#define screen_set(_p, _x, _y)\
-    (_sbuffers[_sback][((_y) * SCREEN_WIDTH + (_x))]=(_p))
+#define screen_buffer() (sbuffers[_sback])
 
-#define screen_offset(_x, _y) (screen_buffer()[(_y) * SCREEN_WIDTH + (_x)])
+#define screen_set(_p, _x, _y) \
+    (((u16*)sbuffers[_sback])[((_y) * SCREEN_WIDTH + (_x))] = (_p))
 
-#define screen_fill(_c, _x, _y, _w, _h) do {\
-        __typeof__(_x) __x = (_x);\
-        __typeof__(_y) __y = (_y);\
-        __typeof__(_w) __w = (_w);\
-        __typeof__(_y) __ymax = __y + (_h);\
-        __typeof__(_c) __c = (_c);\
-        for (; __y < __ymax; __y++) {\
-            memset(&screen_buffer()[__y * SCREEN_WIDTH + __x], __c, __w);\
-        }\
-    } while (0)
 
+#define screen_offset(_x, _y) \
+    (&((u16*)screen_buffer())[(_y) * SCREEN_WIDTH + (_x)])
+
+
+#define screen_fill(_c, _x, _y, _w, _h) do { \
+    typeof(_x) __x = (_x); \
+    typeof(_y) __y = (_y); \
+    typeof(_w) __w = (_w); \
+    typeof(_h) __h = (_h); \
+    typeof(_c) __c = (_c); \
+    u16 *buffer = (u16*)screen_buffer(); \
+    for (typeof(_y) row = 0; row < __h; row++) { \
+        for (typeof(_x) col = 0; col < __w; col++) { \
+            buffer[((__y + row) * SCREEN_WIDTH + (__x + col))] = __c; \
+        } \
+    } \
+} while (0)
+
+// Function declarations
 void screen_swap();
-void screen_clear(u8 color);
+void screen_clear(u16 color);  
 void screen_init();
 int setVideoMode(u16 mode);
 
-int COLOR(int _r, int  _b, int _g);
+
+// red: 5 bits, green: 6 bits, blue: 5 bits
+#define COLOR(_r, _g, _b) \
+    ((u16)((((_r) & 0x1F) << 11) | (((_g) & 0x3F) << 5) | ((_b) & 0x1F)))
+
+
+#define COLOR_RGB888(_r, _g, _b) \
+    ((u16)(((((_r) >> 3) & 0x1F) << 11) | ((((_g) >> 2) & 0x3F) << 5) | (((_b) >> 3) & 0x1F)))
+
+
 
 #ifdef __cplusplus
 }
