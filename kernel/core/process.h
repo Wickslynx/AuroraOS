670b05b7555c71f3c13189453fb833b4d6a178ec
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-06-05 13:24:56 +0200

    wada

---
+++ kernel/core/process.h
@@ -0,0 +1,30 @@
+#ifndef PROCESS_H
+#define PROCESS_H
+
+#include "error.h"
+#include "util.h"
+
+#include "domain.h"
+#include "cpu.h"
+
+typedef enum {
+  RUNNING,
+  READY,
+  TERMINATED,
+  BLOCKED;
+} proc_state_t;
+
+typedef struct {
+  int pid;
+  int ppid;
+  int cpid[100];
+  int exitc;
+  u32 usp; // user stack pointer
+  u32 ksp; // kernel stack pointer.
+  proc_state_t proc_state;
+  Domain domain;
+  page_dir_t page_dir;
+  cpu_state_t cpu_t;
+} Process;
+
+#endif
