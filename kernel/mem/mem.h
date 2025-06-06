a63f6ab1f8e31311e729603b898b48ecee488d61
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-06-06 07:53:50 +0200

    awd

--- kernel/mem/mem.h
+++ kernel/mem/mem.h
@@ -1,8 +1,16 @@
 #ifndef MEM_H
 #define MEM_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include "../core/util.h"
 
 void* malloc(u32 size);
 void free(void* ptr);
+
+#ifdef __cplusplus
+}
+#endif
 #endif
