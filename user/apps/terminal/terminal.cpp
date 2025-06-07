b0b52de2d5803be1f506396fcedc7538882f63ef
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-06-07 09:20:54 +0200

    awd

--- user/apps/terminal/terminal.cpp
+++ user/apps/terminal/terminal.cpp
@@ -15,7 +15,7 @@ int terminal() {
   int len = 0;
 
   Auro::Internal::drawRectangle(COLOR(5, 10, 5), 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
-  Auro::Internal::drawText(" -$ ", 0, 0, COLOR(255, 255, 255), 15);
+  Auro::Internal::drawText(" -$ ", 0, 0, COLOR(255, 255, 255), 3);
   char key = keyboard_get_last_char();
   
   
@@ -30,7 +30,7 @@ int terminal() {
     if (!key == 0) {
     	char buffer[32]; // one char
     	itoa(key, buffer, sizeof(key));
-    	Auro::Internal::drawText(buffer, twidth+2, 10, COLOR(7, 7, 3));
+    	Auro::Internal::drawText(buffer, twidth+2, 10, COLOR(7, 7, 3), 3);
     	len++;
     }
   }
