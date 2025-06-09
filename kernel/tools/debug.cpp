54e2aa0855b2f58335b08d92547cdf0b79a8b5a3
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-06-09 18:52:20 +0200

    I need processes ASAp

--- kernel/tools/debug.cpp
+++ kernel/tools/debug.cpp
@@ -4,8 +4,8 @@
 
 static inline void term_print(const char* msg) {
   while (*msg) {
-    if (msg == "\n") {
-      serial_write("\r");
+    if (msg == '\n') {
+      serial_write('\r');
     }
 
     serial_write(*msg++);
@@ -17,27 +17,29 @@ static inline void donun() {
 }
 
 void debug_terminal() {
-  term_print("AuroraOS - Debugger, type <help>? ");
+  term_print("AuroraOS - Debugger, type <help>? \r");
   term_print("\033[31m admin@auroraos -> \033[0m");
-  const char* cmd = serial_readline();
+  char cmd[256];
+  serial_readline(cmd, 256);
 
   // for now i'll hardcode commands.
-  if (cmd == 'help') {
-    term_print("List of available commands: \n ls - list all files in your current directory. \n cd - change into a directory, use .. to go backwards. \n proc - view all active processes.");
-  } else if (strcmp(cmd, 'ls') == 0) {
+  if (cmd == "help") {
+    term_print("List of available commands: \n ls - list all files in your current directory. \n cd - change into a directory, use .. to go backwards. \n proc - view all active processes. \n run - run a .elf file");
+  } else if (strcmp(cmd, "ls") == 0) {
     // ehnmm.. need to do other stuff before, ill ignore this.
     donun();
-  } else if (strcmp(cmd, 'cd') == 0) {
+  } else if (strcmp(cmd, "cd") == 0) {
     // no directories either.
     donun();
-  } else if (strcmp(cmd, 'proc') == 0) {
+  } else if (strcmp(cmd, "proc") == 0) {
     // processes are not fully implemented. no task manager either.
     donun();
-    ternm_print("Tasks:\n There are no current tasks running..");
-  } else if (strcmp(cmd, 'exit') == 0) {
+    term_print("Tasks:\n There are no current tasks running..");
+  } else if (strcmp(cmd, "exit") == 0) {
     for (;;) {}; // run forever.. Do nothing.
-  } else if (strcmp(cmd, 'run') == 0) {
+  } else if (strcmp(cmd, "run") == 0) {
     // i need processes for this, rn this whole thing is a proof of conecpt.
+    donun();
   }
 
   
