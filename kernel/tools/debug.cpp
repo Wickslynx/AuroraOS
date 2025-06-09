8917ad944c842e07ec98cc0955bce0db29722463
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-06-09 18:42:25 +0200

    Fix

--- kernel/tools/debug.cpp
+++ kernel/tools/debug.cpp
@@ -19,23 +19,24 @@ static inline void donun() {
 void debug_terminal() {
   term_print("AuroraOS - Debugger, type <help>? ");
   term_print("\033[31m admin@auroraos -> \033[0m");
-  const char* cmd = serial_read();
+  const char* cmd = serial_readline();
 
   // for now i'll hardcode commands.
   if (cmd == 'help') {
     term_print("List of available commands: \n ls - list all files in your current directory. \n cd - change into a directory, use .. to go backwards. \n proc - view all active processes.");
-  } else if (cmd == 'ls') {
+  } else if (strcmp(cmd, 'ls') == 0) {
     // ehnmm.. need to do other stuff before, ill ignore this.
     donun();
-  } else if (cmd == 'cd') {
+  } else if (strcmp(cmd, 'cd') == 0) {
     // no directories either.
     donun();
-  } else if (cmd == 'proc') {
+  } else if (strcmp(cmd, 'proc') == 0) {
     // processes are not fully implemented. no task manager either.
     donun();
-  } else if (cmd == 'exit') {
+    ternm_print("Tasks:\n There are no current tasks running..");
+  } else if (strcmp(cmd, 'exit') == 0) {
     for (;;) {}; // run forever.. Do nothing.
-  } else if (cmd == 'run') {
+  } else if (strcmp(cmd, 'run') == 0) {
     // i need processes for this, rn this whole thing is a proof of conecpt.
   }
 
