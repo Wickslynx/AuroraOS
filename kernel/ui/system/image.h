0d6d9f9a139458cbf72febc6e12a216fe4194cfd
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-06-17 16:34:58 +0200

    Fix

--- kernel/ui/system/image.h
+++ kernel/ui/system/image.h
@@ -12,7 +12,7 @@ typedef struct {
     u16 reserved1;      
     u16 reserved2;     
     u32 data_offset;   
-} __attribute__((packed)) bmp_file_t
+} __attribute__((packed)) bmp_file_t;
 
 typedef struct {
     u32 header_size;    // 40
@@ -26,7 +26,9 @@ typedef struct {
     u32 y_pixels_per_m; 
     u32 colors_used;    // num of colors used
     u32 colors_important; // num of important colors
-} __attribute__((packed)) bmp_info_t
+} __attribute__((packed)) bmp_info_t;
+
+int image_bmp(const u8 *bmp_data, u32 bmp_size, int x, int y);
 
 #ifdef __cplusplus
 }
