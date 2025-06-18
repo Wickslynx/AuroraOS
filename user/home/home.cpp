e2032cce9049628fd3a843b1148a94064592fb8c
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-06-18 16:17:23 +0200

    adw

--- user/home/home.cpp
+++ user/home/home.cpp
@@ -7,7 +7,6 @@
 
 
 int __home() { // will be main later 
-  LOG_INFO(" Starting main menu..");
   //Auro::Internal::drawRectangle(COLOR(0, 0, 60), 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0); // bg
   //Auro::Internal::drawText("12:00", SCREEN_WIDTH-SCREEN_WIDTH/8, SCREEN_HEIGHT/41, COLOR(0, 0, 0), 3); // time
   //Auro::Internal::drawRectangle(COLOR(236, 236, 236), SCREEN_WIDTH/20*17.9, SCREEN_HEIGHT/16,  SCREEN_WIDTH/10, SCREEN_HEIGHT/10*9, 15); // right top down bar. (files, Terminal, Notes)
