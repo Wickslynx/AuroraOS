985bcfc3d488685fa623a98bf296828772f79527
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-06-18 15:19:58 +0200

    Uploading from my own PC

--- kernel/fs/disk.h
+++
@@ -1,8 +0,0 @@
-typedef struct {
-  int (*read)(u32 dev, u32 sector, void *buffer);
-  int (*write)(u32 dev, u32 sector, const void *buffer);
-  int (*flush)(u32 dev); 
-} disk;
-
-int readb();
-int writeb();
