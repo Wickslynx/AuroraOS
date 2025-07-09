42120cb63451669b80f5e26af463fd0663edc626
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-07-09 16:13:13 +0200

    Fix

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -8,9 +8,34 @@
 #include "mem/vmm.h"
 #include "ui/system/screen.h"
 #include "../user/home/home.cpp"
+#include "core/process.h"
+#include "core/domain"
+
+
+u8 simple_program[] = {
+    0xEB, 0xFE  // JMP $
+};
 
 void test() {
 	__home();
+	u32 fake_entry_addr = 0x400000;  // Arbitrary user address
+	u32 fake_stack_addr = 0x800000;  // Arbitrary user stack top
+	
+	// create some test proc.
+	Process* test_proc = process_create("test_proc", 0, Domain domain, fake_entry_addr, fake_stack_addr);
+	if (!test_proc) {
+	    LOG_ERROR("Failed to create test process");
+	    return;
+	}
+	
+	 // load the test program
+	if (load_program(test_proc, simple_program, sizeof(simple_program)) != 0) {
+	    LOG_ERROR("Failed to load test program");
+	    return;
+	}
+	
+	// for now - simulate the scheduler.. This will be done automatically once i enable the scheduler.
+	scheduler();
 }
     
 extern "C" void _main(u32 magic) {
